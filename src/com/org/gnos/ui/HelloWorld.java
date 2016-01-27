1fa9f362cde0a7ea769a2f0b662285289dfa72c0
rohghosh <rohan.ghosh@hotmail.com>
2016-01-27 20:10:30 +0530

    Enhancing UI

--- src/com/org/gnos/ui/HelloWorld.java
+++ src/com/org/gnos/ui/HelloWorld.java
@@ -2,10 +2,14 @@ package com.org.gnos.ui;
 
 import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 
 public class HelloWorld extends ApplicationWindow{
 
@@ -13,10 +17,19 @@ public class HelloWorld extends ApplicationWindow{
 		super(null);
 	}
 	
+	protected void configureShell(Shell newShell) {
+        super.configureShell(newShell);
+        newShell.setText("GNOS - Adding a new dimension to mining");
+    }
+	
 	protected Control createContents(Composite parent) {
 		Composite container = new Composite(parent, SWT.NONE);
-		container.setLayout(new GridLayout(1, true));
-        
+		//container.setLayout(new GridLayout(1, true));
+		container.setLayout(new FillLayout());
+		Image backgroundImage = new Image (Display.getCurrent(), "resources/bg.jpg");
+		container.setBackgroundImage(backgroundImage);
+		container.setBackgroundMode(SWT.INHERIT_FORCE);
+		container.setBounds(0, 0, backgroundImage.getBounds().width, backgroundImage.getBounds().height);
 		
 		
 		Label label = new Label(container, SWT.NONE);
