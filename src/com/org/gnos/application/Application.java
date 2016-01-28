c9da718ce6777acaec338f916d6901c8bf165b23
rohghosh <rohan.ghosh@hotmail.com>
2016-01-28 19:28:06 +0530

    Refactoring screens as separate java classes

--- src/com/org/gnos/application/Application.java
+++ src/com/org/gnos/application/Application.java
@@ -4,18 +4,23 @@ import org.eclipse.swt.widgets.Display;
 
 import com.org.gnos.ui.screens.HelloWorld;
 import com.org.gnos.ui.screens.MultiScreenTest;
+import com.org.gnos.ui.screens.ScreenController;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HelloWorld oHelloWorld = new HelloWorld();
-		/*oHelloWorld.setBlockOnOpen(true);
-		oHelloWorld.open();*/
+		oHelloWorld.setBlockOnOpen(true);
+		/*oHelloWorld.open();*/
 		
 		MultiScreenTest oMultiScreenTest = new MultiScreenTest();
 		oMultiScreenTest.setBlockOnOpen(true);
-		oMultiScreenTest.open();
+		/*oMultiScreenTest.open();*/
+		
+		ScreenController oScreenController = new ScreenController();
+		oScreenController.setBlockOnOpen(true);
+		oScreenController.open();
 
 		Display.getCurrent().dispose();
 	}
