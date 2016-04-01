c40b45eba1f4b11a9ca48d6389abe272cfd8e743
abandyopadhy <arpan.bandyopadhyay@perceptivesoftware.com>
2016-04-01 11:36:19 +0530

    Fixed screen navigation issue

--- src/com/org/gnos/ui/screens/v1/GNOSApplication.java
+++ src/com/org/gnos/ui/screens/v1/GNOSApplication.java
@@ -7,6 +7,7 @@ import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.FormAttachment;
@@ -25,6 +26,7 @@ import com.org.gnos.events.GnosEvent;
 import com.org.gnos.events.GnosEventWithAttributeMap;
 import com.org.gnos.events.interfaces.GnosEventListener;
 import com.org.gnos.services.csv.GNOSCSVDataProcessor;
+import com.org.gnos.ui.custom.controls.GnosScreen;
 
 public class GNOSApplication extends ApplicationWindow implements GnosEventListener{
 
@@ -32,6 +34,11 @@ public class GNOSApplication extends ApplicationWindow implements GnosEventListe
 	//private CTabFolder cTabFolder;
 	//private HomeTabItem homeTabItem;
 	//private ProjectTabItem projectTabItem;
+	private StackLayout stackLayout;
+	private GnosScreen viewPort;
+	private Shell dummyShell;
+	private ProjectScreen projectScreen;
+	private WorkbenchScreen workbenchScreen;
 	private GNOSCSVDataProcessor gnosCsvDataProcessor;
 	/**
 	 * Create the application window.
@@ -50,73 +57,14 @@ public class GNOSApplication extends ApplicationWindow implements GnosEventListe
 	 */
 	@Override
 	protected Control createContents(Composite parent) {
-		container = new Composite(parent, SWT.NONE);
-		container.setLayout(new FormLayout());
-		
-		CLabel labelProject = new CLabel(container, SWT.NONE);
-		labelProject.setForeground(SWTResourceManager.getColor(255, 255, 255));
-		labelProject.setFont(SWTResourceManager.getFont("Arial", 12, SWT.NORMAL));
-		labelProject.setBackground(SWTResourceManager.getColor(0, 102, 204));
-		FormData fd_labelProject = new FormData();
-		fd_labelProject.bottom = new FormAttachment(0, 34);
-		fd_labelProject.right = new FormAttachment(100);
-		fd_labelProject.top = new FormAttachment(0);
-		fd_labelProject.left = new FormAttachment(0);
-		labelProject.setLayoutData(fd_labelProject);
-		labelProject.setText("Projects");
-		
-		Label label = new Label(container, SWT.SEPARATOR | SWT.VERTICAL);
-		FormData fd_label = new FormData();
-		fd_label.left = new FormAttachment(0, 260);
-		fd_label.top = new FormAttachment(labelProject, 6);
-		fd_label.bottom = new FormAttachment(100, 10);
-		fd_label.right = new FormAttachment(0, 262);
-		label.setLayoutData(fd_label);
-		
-		CLabel lastLabel = labelProject;
-		for(int i=0; i< 10; i++){
-			CLabel labelProject1 = new CLabel(container, SWT.NONE);
-			labelProject1.setForeground(SWTResourceManager.getColor(255, 255, 255));
-			labelProject1.setFont(SWTResourceManager.getFont("Arial", 12, SWT.NORMAL));
-			labelProject1.setBackground(SWTResourceManager.getColor(0, 102, 204));
-			FormData fd_labelProject1 = new FormData();
-			fd_labelProject1.bottom = new FormAttachment(lastLabel, 40, SWT.BOTTOM);
-			fd_labelProject1.right = new FormAttachment(label, -6);
-			fd_labelProject1.top = new FormAttachment(lastLabel, 6);
-			fd_labelProject1.left = new FormAttachment(0, 10);
-			labelProject1.setLayoutData(fd_labelProject1);
-			labelProject1.setText("Project "+(i+1));
-			lastLabel = labelProject1;
-		}
+		container = new Composite(parent, SWT.NONE);		
+		this.stackLayout = new StackLayout();
+		container.setLayout(stackLayout);
+		this.dummyShell = new Shell();
 
-		ScrolledComposite scViewPortContainer = new ScrolledComposite(container, SWT.V_SCROLL | SWT.NONE);
-		FormData fd_scViewPortContainer = new FormData();
-		fd_scViewPortContainer.right = new FormAttachment(labelProject, -6, SWT.RIGHT);
-		fd_scViewPortContainer.bottom = new FormAttachment(100, -6);
-		fd_scViewPortContainer.top = new FormAttachment(labelProject, 6);
-		fd_scViewPortContainer.left = new FormAttachment(label, 6);
-
-		CreateNewProjectScreen newProjectScreen = new CreateNewProjectScreen(scViewPortContainer, SWT.BORDER);
-
-		scViewPortContainer.setContent(newProjectScreen);
-		scViewPortContainer.setExpandHorizontal(true);
-		scViewPortContainer.setExpandVertical(true);
-		scViewPortContainer.setLayout(new FillLayout());
-		scViewPortContainer.setLayoutData(fd_scViewPortContainer);
-		
-		newProjectScreen.registerEventListener(this);
-		//cTabFolder = new CTabFolder(container, SWT.NONE);
-		//cTabFolder.setSelectionBackground(Display.getCurrent().getSystemColor(SWT.COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT));
-/*		FormData fd_tabFolder = new FormData();
-		fd_tabFolder.bottom = new FormAttachment(100, -10);
-		fd_tabFolder.left = new FormAttachment(0, 10);
-		fd_tabFolder.top = new FormAttachment(0, 23);
-		fd_tabFolder.right = new FormAttachment(100, -10);*/
-		//tabFolder.setLayoutData(fd_tabFolder);
-		//cTabFolder.setLayoutData(fd_tabFolder);
-
-		//homeTabItem = new HomeTabItem(cTabFolder, SWT.NONE);
-		//homeTabItem.registerEventListener(this);
+		projectScreen = new ProjectScreen(this.dummyShell, SWT.NONE);
+		projectScreen.registerEventListener(this);
+		loadProjectScreen();
 
 		
 		Label label_1 = new Label(container, SWT.NONE);
@@ -128,6 +76,29 @@ public class GNOSApplication extends ApplicationWindow implements GnosEventListe
 		return container;
 	}
 
+	private void loadProjectScreen() {
+		if(this.viewPort != null){
+			//this.viewPort.dispose();
+			this.viewPort.setParent(dummyShell);
+		}
+		this.viewPort = projectScreen;
+		this.viewPort.setParent(container);
+		this.stackLayout.topControl = this.viewPort;
+		container.layout();
+	}
+	
+	private void loadWorkBenchScreen(ProjectModel projectModel) {
+		workbenchScreen = new WorkbenchScreen(this.dummyShell, SWT.NONE, projectModel);
+		workbenchScreen.registerEventListener(this);
+		if(this.viewPort != null){
+			//this.viewPort.dispose();
+			this.viewPort.setParent(dummyShell);
+		}
+		this.viewPort = workbenchScreen;
+		this.viewPort.setParent(container);
+		this.stackLayout.topControl = this.viewPort;
+		container.layout();
+	}
 	/**
 	 * Create the actions.
 	 */
@@ -204,7 +175,6 @@ public class GNOSApplication extends ApplicationWindow implements GnosEventListe
 			GnosEventWithAttributeMap event = (GnosEventWithAttributeMap)e;
 			openPitControlsTab(event);
 		} else if(e.eventName == "createNewProjectScreen:upload-records-complete") {
-			System.out.println("New project event fired");
 			GnosEventWithAttributeMap event = (GnosEventWithAttributeMap)e;
 			openPitControlsTab(event);
 		}
@@ -225,8 +195,8 @@ public class GNOSApplication extends ApplicationWindow implements GnosEventListe
 		gnosCsvDataProcessor.processCsv(event.attributes.get("recordFileName"));
 		
 		projectModel.setAllProjectFields(gnosCsvDataProcessor.getHeaderColumns());
-		
-		container.setLayoutData(new WorkbenchScreen(container, SWT.NONE, projectModel));
+		loadWorkBenchScreen(projectModel);
+		//container.setLayoutData(new WorkbenchScreen(container, SWT.NONE, projectModel));
 		//projectTabItem = new ProjectTabItem(cTabFolder, SWT.CLOSE, projectModel);
 		//projectTabItem.registerEventListener(this);
 		//cTabFolder.setSelection(projectTabItem);
