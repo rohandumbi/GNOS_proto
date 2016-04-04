cbda0f7010e5c62f52ce3ec4b339a62a0e10d770
Rohan Ghosh <rohan.dumb@gmail.com>
2016-04-05 00:51:54 +0530

    Added styling to process definition dialog. Populating process combo with all available models. Added delete feature for a process step

--- src/com/org/gnos/ui/screens/v1/ProcessDefinitionFormScreen.java
+++ src/com/org/gnos/ui/screens/v1/ProcessDefinitionFormScreen.java
@@ -1,26 +1,31 @@
 package com.org.gnos.ui.screens.v1;
 
-import org.eclipse.swt.widgets.Composite;
+import java.util.List;
 
-import com.org.gnos.events.GnosEvent;
-import com.org.gnos.ui.custom.controls.GnosScreen;
-
-import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Combo;
-import org.eclipse.wb.swt.SWTResourceManager;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.wb.swt.SWTResourceManager;
+
+import com.org.gnos.events.GnosEvent;
+import com.org.gnos.services.Model;
+import com.org.gnos.services.Models;
+import com.org.gnos.ui.custom.controls.GnosScreen;
 
 public class ProcessDefinitionFormScreen extends GnosScreen {
 	private Text textProcessName;
@@ -28,38 +33,39 @@ public class ProcessDefinitionFormScreen extends GnosScreen {
 	private Composite lastProcessStep;
 	private Composite modelListContainerComposite;
 	private ScrolledComposite scViewportContainer;
+	private String[] sourceFieldsComboItems;
 
 	public ProcessDefinitionFormScreen(Composite parent, int style) {
 		super(parent, style);
-		setBackground(SWTResourceManager.getColor(240, 248, 255));
+		//setBackground(SWTResourceManager.getColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT));
 		setLayout(new FormLayout());
-		
+
 		Label lblProcessName = new Label(this, SWT.NONE);
-		lblProcessName.setBackground(SWTResourceManager.getColor(240, 248, 255));
+		//lblProcessName.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblProcessName.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
 		FormData fd_lblProcessName = new FormData();
 		fd_lblProcessName.top = new FormAttachment(0, 37);
 		fd_lblProcessName.left = new FormAttachment(0, 44);
 		lblProcessName.setLayoutData(fd_lblProcessName);
 		lblProcessName.setText("Process Name:");
-		
+
 		textProcessName = new Text(this, SWT.BORDER);
 		FormData fd_textProcessName = new FormData();
 		fd_textProcessName.right = new FormAttachment(lblProcessName, 522, SWT.RIGHT);
 		fd_textProcessName.top = new FormAttachment(0, 37);
 		fd_textProcessName.left = new FormAttachment(lblProcessName, 6);
 		textProcessName.setLayoutData(fd_textProcessName);
-		
+
 		Label lblChooseColor = new Label(this, SWT.NONE);
 		lblChooseColor.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
-		lblChooseColor.setBackground(SWTResourceManager.getColor(240, 248, 255));
+		//lblChooseColor.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		FormData fd_lblChooseColor = new FormData();
 		//fd_lblChooseColor.bottom = new FormAttachment(lblStep, -89);
 		fd_lblChooseColor.left = new FormAttachment(lblProcessName, 0, SWT.LEFT);
 		fd_lblChooseColor.top = new FormAttachment(lblProcessName, 20, SWT.BOTTOM);
 		lblChooseColor.setLayoutData(fd_lblChooseColor);
 		lblChooseColor.setText("Choose Color:");
-		
+
 		Button btnNewButton = new Button(this, SWT.NONE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -76,7 +82,7 @@ public class ProcessDefinitionFormScreen extends GnosScreen {
 		fd_btnNewButton.left = new FormAttachment(0, 44);
 		btnNewButton.setLayoutData(fd_btnNewButton);
 		btnNewButton.setText("Add Model Step");
-		
+
 		this.labelVerticalSeparator = new Label(this, SWT.SEPARATOR | SWT.HORIZONTAL);
 		FormData fd_labelVerticalSeparator = new FormData();
 		fd_labelVerticalSeparator.bottom = new FormAttachment(btnNewButton, 22, SWT.BOTTOM);
@@ -84,25 +90,25 @@ public class ProcessDefinitionFormScreen extends GnosScreen {
 		fd_labelVerticalSeparator.top = new FormAttachment(btnNewButton, 20);
 		fd_labelVerticalSeparator.left = new FormAttachment(0);
 		this.labelVerticalSeparator.setLayoutData(fd_labelVerticalSeparator);
-		
+
 		this.scViewportContainer = new ScrolledComposite(this, SWT.V_SCROLL | SWT.NONE);
 		FormData fd_scViewPortContainer = new FormData();
-		fd_scViewPortContainer.top = new FormAttachment(this.labelVerticalSeparator, 10, SWT.BOTTOM);
+		fd_scViewPortContainer.top = new FormAttachment(this.labelVerticalSeparator, 0, SWT.BOTTOM);
 		fd_scViewPortContainer.bottom = new FormAttachment(100);
 		fd_scViewPortContainer.left = new FormAttachment(0);
 		fd_scViewPortContainer.right = new FormAttachment(100);
-		
+
 		this.modelListContainerComposite = new Composite(this.scViewportContainer, SWT.NONE);
 		this.modelListContainerComposite.setLayout(new FormLayout());
-		this.modelListContainerComposite.setBackground(SWTResourceManager.getColor(240, 248, 255));
-		
+		this.modelListContainerComposite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
 		FormData fd_modelListContainerComposite = new FormData();
-		fd_modelListContainerComposite.top = new FormAttachment(this.labelVerticalSeparator, 10, SWT.BOTTOM);
+		fd_modelListContainerComposite.top = new FormAttachment(this.labelVerticalSeparator, 0, SWT.BOTTOM);
 		fd_modelListContainerComposite.bottom = new FormAttachment(100);
 		fd_modelListContainerComposite.left = new FormAttachment(0);
 		fd_modelListContainerComposite.right = new FormAttachment(100);
 		this.modelListContainerComposite .setLayoutData(fd_modelListContainerComposite);
-		
+
 		this.scViewportContainer.setContent(this.modelListContainerComposite);
 		this.scViewportContainer.setExpandHorizontal(true);
 		this.scViewportContainer.setExpandVertical(true);
@@ -117,15 +123,22 @@ public class ProcessDefinitionFormScreen extends GnosScreen {
 			}
 		});
 		this.scViewportContainer.setLayoutData(fd_scViewPortContainer);
-		
-		
-		
-		// TODO Auto-generated constructor stub
 	}
-	
+
+	private String[] getSourceFieldsComboItems(){
+
+		List<Model> models = Models.getAll();
+		this.sourceFieldsComboItems = new String[models.size()];
+		for(int i=0; i<models.size(); i++){
+			this.sourceFieldsComboItems[i] = models.get(i).getName();
+		}
+
+		return this.sourceFieldsComboItems;
+	}
+
 	private void addModelStep(){
-		Composite processStep = new Composite(this.modelListContainerComposite, SWT.None);
-		processStep.setBackground(SWTResourceManager.getColor(240, 248, 255));
+		final Composite processStep = new Composite(this.modelListContainerComposite, SWT.None);
+		processStep.setBackground(SWTResourceManager.getColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT));
 		processStep.setLayout(new FormLayout());
 		FormData fd_processStep = new FormData();
 		fd_processStep.left =  new FormAttachment(0);
@@ -133,33 +146,67 @@ public class ProcessDefinitionFormScreen extends GnosScreen {
 		if(this.lastProcessStep != null){
 			fd_processStep.top = new FormAttachment(this.lastProcessStep, 10, SWT.BOTTOM);
 		}else{
-			fd_processStep.top = new FormAttachment(this.labelVerticalSeparator, 22, SWT.BOTTOM);
-			/*fd_processStep.top = new FormAttachment(0);*/
+			fd_processStep.top = new FormAttachment(this.labelVerticalSeparator, 10, SWT.BOTTOM);
 		}
 		processStep.setLayoutData(fd_processStep);
-		
+
 		Label lblStep = new Label(processStep, SWT.NONE);
 		lblStep.setText("Step Name:");
-		lblStep.setBackground(SWTResourceManager.getColor(240, 248, 255));
+		lblStep.setBackground(SWTResourceManager.getColor(SWT.COLOR_TITLE_BACKGROUND_GRADIENT));
 		lblStep.setFont(SWTResourceManager.getFont("Segoe UI", 12, SWT.NORMAL));
 		FormData fd_lblStep = new FormData();
 		fd_lblStep.left = new FormAttachment(0, 44);
+		fd_lblStep.top = new FormAttachment(0, 5);
 		lblStep.setLayoutData(fd_lblStep);
-		
-		Combo comboStep = new Combo(processStep, SWT.NONE);
+
+		final Combo comboStep = new Combo(processStep, SWT.NONE);
 		FormData fd_comboStep = new FormData();
 		//fd_comboStep.top = new FormAttachment(lblStep, 2, SWT.TOP);
 		fd_comboStep.left = new FormAttachment(lblStep, 5, SWT.RIGHT);
 		fd_comboStep.right = new FormAttachment(50);
+		fd_comboStep.top = new FormAttachment(0, 5);
 		comboStep.setLayoutData(fd_comboStep);
+		comboStep.setItems(getSourceFieldsComboItems());
+		comboStep.setText("====Select Model====");
+		comboStep.addListener(SWT.MouseDown, new Listener(){
+			@Override
+			public void handleEvent(Event event) {
+				// TODO Auto-generated method stub
+				//System.out.println("detected combo click");
+				comboStep.removeAll();
+				comboStep.setItems(getSourceFieldsComboItems());
+				comboStep.getParent().layout();
+				comboStep.setListVisible(true);
+			}
+		});
 		
+		Button deleteButton = new Button(processStep, SWT.NONE);
+		deleteButton.setImage(SWTResourceManager.getImage(ProcessDefinitionFormScreen.class, "/com/org/gnos/resources/trash.png"));
+		FormData fd_deleteButton = new FormData();
+		fd_deleteButton.left = new FormAttachment(comboStep, 5, SWT.RIGHT);
+		//fd_deleteButton.top = new FormAttachment(comboStep, 0, SWT.TOP);
+		//fd_deleteButton.bottom = new FormAttachment(comboStep, 0, SWT.BOTTOM);
+		deleteButton.setLayoutData(fd_deleteButton);
+		deleteButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//addModelStep();
+				processStep.dispose();
+				modelListContainerComposite.layout();
+				adjustScrollableViewport();
+			}
+		});
+
 		this.lastProcessStep = processStep;
 		this.modelListContainerComposite.layout();
 		
+		this.adjustScrollableViewport();
+	}
+	
+	private void adjustScrollableViewport(){
 		Rectangle r = this.scViewportContainer.getClientArea();
 		this.scViewportContainer.setMinSize(this.modelListContainerComposite.computeSize(r.width, SWT.DEFAULT));
 		this.scViewportContainer.setOrigin(this.modelListContainerComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-		//this.layout();
 	}
 
 	@Override
