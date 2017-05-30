81217cc691c6194e14c06046785877af66953c1c
abandyopadhy <arpan.bandyopadhyay@perceptivesoftware.com>
2017-05-30 18:45:11 +0530

    Implemented the multicsv functionality

--- src/com/org/gnos/services/controller/ProjectController.java
+++ src/com/org/gnos/services/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.org.gnos.services.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.JsonArray;
@@ -8,7 +9,7 @@ import com.google.gson.JsonObject;
 import com.org.gnos.db.dao.ProjectDAO;
 import com.org.gnos.db.model.Project;
 import com.org.gnos.services.PitBenchProcessor;
-import com.org.gnos.services.csv.GNOSCSVDataProcessor;
+import com.org.gnos.services.util.FileUploadHelper;
 
 public class ProjectController {
 	
@@ -23,24 +24,44 @@ public class ProjectController {
 	}
 	
 	public Project create(JsonObject jsonObject) throws Exception {
-			String name = jsonObject.get("name").getAsString();
-			String desc = jsonObject.get("desc").getAsString();
-			JsonArray files = jsonObject.get("files").getAsJsonArray();
-			Project obj = new Project();
-			obj.setName(name);
-			obj.setDesc(desc);
-			
+		
+		FileUploadHelper helper = null;
+		String name = jsonObject.get("name").getAsString();
+		String desc = jsonObject.get("desc").getAsString();
+		JsonArray files = jsonObject.get("files").getAsJsonArray();
+		Project obj = new Project();
+		obj.setName(name);
+		obj.setDesc(desc);
+		
+		List<String> fileNames = new ArrayList<String>();
+		if(files.size() > 0) {
 			for(JsonElement file: files) {
-				obj.addFile(file.getAsString());
+				fileNames.add(file.getAsString());
 			}
+			obj.setFiles(fileNames);
 			
-			boolean created = dao.create(obj);
-			//loadCSVFile(fileName, newProject.getId());
-			if(created) return obj;
-			throw new Exception();
+			helper = new FileUploadHelper(fileNames);
+			boolean correct = helper.verifyHeaders();
+			
+			if(!correct) {
+				throw new Exception("File headers are not matching.");
+			}
+		}
+		
+		boolean created = dao.create(obj);
+		
+		if(created && helper != null){
+			helper.storeFields(obj.getId());
+			helper.loadData(obj.getId(), false);
+			new PitBenchProcessor().updatePitBenchData(obj.getId());			
+			return obj;
+		}
+		throw new Exception();
 	}
 	
 	public Project update(JsonObject jsonObject, String projectId) throws Exception {
+		
+		FileUploadHelper helper = null;
 		String name = jsonObject.get("name").getAsString();
 		String desc = jsonObject.get("desc").getAsString();
 		boolean append = jsonObject.get("append").getAsBoolean();
@@ -51,13 +72,26 @@ public class ProjectController {
 		obj.setName(name);
 		obj.setDesc(desc);
 
-		for(JsonElement file: files) {
-			obj.addFile(file.getAsString());
+		List<String> fileNames = new ArrayList<String>();
+		if(files.size() > 0) {
+			for(JsonElement file: files) {
+				fileNames.add(file.getAsString());
+			}
+			obj.setFiles(fileNames);
+			helper = new FileUploadHelper(fileNames);
+			boolean correct = helper.verifyHeaders();		
+			if(!correct) {
+				throw new Exception("File headers are not matching.");
+			}
 		}
 		
-		boolean created = dao.update(obj, append);
-		//loadCSVFile(fileName, newProject.getId());
-		if(created) return obj;
+		boolean updated = dao.update(obj, append);
+		
+		if(updated && helper != null) {
+			helper.loadData(obj.getId(), append);
+			new PitBenchProcessor().updatePitBenchData(obj.getId());			
+			return obj;
+		}
 		throw new Exception();
 	}
 
@@ -71,13 +105,11 @@ public class ProjectController {
 			return true;
 		}	
 	}
-	
-	
-	private void loadCSVFile(String fileName, int projectId ) {
-		GNOSCSVDataProcessor gnosCsvDataProcessor = new GNOSCSVDataProcessor();
+
+/*	private void loadCSVFile(String fileName, int projectId ) {
 		gnosCsvDataProcessor.processCsv(fileName);
 		gnosCsvDataProcessor.storeFields(projectId);
 		gnosCsvDataProcessor.dumpToDB(projectId);
 		new PitBenchProcessor().updatePitBenchData(projectId);
-	}
+	}*/
 }
