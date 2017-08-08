a29e00d0bafc6d34b03b43b12b56a5d67b16c7da
Arpan <kalpurus.hos@gmail.com>
2017-08-08 20:34:28 +0530

    Added support for providing end period less than total no of periods specified in scenario defination

--- src/com/org/gnos/services/controller/SchedulerController.java
+++ src/com/org/gnos/services/controller/SchedulerController.java
@@ -19,17 +19,18 @@ public class SchedulerController {
 			short mode = jsonObject.get("mode").getAsShort();
 			boolean isReclaim = jsonObject.get("isReclaim").getAsBoolean();
 			float gap = jsonObject.get("gap").getAsFloat();
+			short period = jsonObject.get("period").getAsShort();
+			
 			runconfig.setMode(mode);
 			runconfig.setReclaim(isReclaim);
+			runconfig.setPeriod(period);			
+			
 			if(gap > 0 && gap <= 100) {
 				runconfig.setMIPGAP(gap/100);
 			}
-			if(mode == 2) {
-				short period = jsonObject.get("period").getAsShort();
+			if(mode == 2) {				
 				short window = jsonObject.get("window").getAsShort();
-				short stepSize = jsonObject.get("stepSize").getAsShort();
-				
-				runconfig.setPeriod(period);
+				short stepSize = jsonObject.get("stepSize").getAsShort();			
 				runconfig.setWindow(window);
 				runconfig.setStepSize(stepSize);
 
